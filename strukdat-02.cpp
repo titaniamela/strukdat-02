3445c29313c3e39878d9fede9463a4bca3b6ee82
titaniamela <shaniaslsbl@gmail.com>
2019-03-06 10:46:44 +0700

    strukdat-02

---
+++ strukdat-02.cpp
@@ -0,0 +1,38 @@
+/*
+Nama        : Shania Salsabila
+NPM         : 140810180014
+Kelas	    : B
+Deskripsi   : pretest
+Tanggal     : 6 Maret 2019
+*/
+#include <iostream>
+
+using namespace std;
+
+void moveZeroTofront (int arr[], int n){}
+int count=0
+for(int i=o, i<n;i++)
+    if(arr )
+
+void input (int (&arr)[100], int& n){}
+void output (int arr[], int n){}
+void swap (int& x, int& y){
+      int temp = x;
+x = y;
+y = temp; }
+void bubbleSort(larik& a, int n) {
+      for (int i=n-1; i > 0; i--){
+            for (int j=0; j < i; j++) {
+                  if (a[j] > a[j+1] )
+}
+} }
+swap (a[j], a[j+1]);
+int main(){
+    int arr[100];
+    int n;
+    input(arr,n);
+    moveZeroToFront(arr,n);
+    output(arr,n);
+
+
+}
